53b19676c7bdf0ee95aac102c736e3817b0b41bf
LuisBaes <93173785+LuisBaes@users.noreply.github.com>
2021-12-02 15:21:51 -0700

    Add files via upload

---
+++ Dominio/Dominio/src/Dominio/Punto.java
@@ -0,0 +1,14 @@
+/**
+ * Punto.java
+ */
+package Dominio;
+
+/**
+ *
+ * @author German Verdugo, Luis Baes
+ */
+public class Punto extends Forma{
+    public Punto(int width, int height, int x, int y) {
+        super(width, height, x, y);
+    }  
+}
---
+++ Dominio/DominioDTO/src/DominioDTO/MensajeSockets.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DominioDTO;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author German Verdugo, Luis Baes
+ */
+public enum MensajeSockets implements Serializable {
+    VOTO, CREAR_SALA, EMPEZAR_PARTIDA, JUGADOR_NUEVO, TURNO_TERMINADO, MARCADOR
+}
