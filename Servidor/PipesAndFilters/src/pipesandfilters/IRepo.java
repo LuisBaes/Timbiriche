954bd9a3001a4af8ad8cd89875233126d3acc366
LuisBaes <93173785+LuisBaes@users.noreply.github.com>
2021-12-02 15:26:04 -0700

    Add files via upload

---
+++ Servidor/PipesAndFilters/src/pipesandfilters/IRepo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pipesandfilters;
+
+import DominioDTO.CuadroDTO;
+import DominioDTO.LineaDTO;
+import DominioDTO.MarcadorDTO;
+
+/**
+ *
+ * @author German Verdugo, Luis Baes
+ */
+public interface IRepo {
+    MarcadorDTO obtenerMarcador();
+    LineaDTO obtenerUltimaLinea();
+    CuadroDTO obtenerUltimoCuadro();
+    int obtenerTurnoSiguiente();
+}
